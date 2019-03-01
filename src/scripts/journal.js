7a2c04e49bda039b3138178016627d5a07dc80c5
Josh Hibray <jjhibray@gmail.com>
2019-03-01 15:24:38 -0600

    finished daily-journal-2

---
+++ src/scripts/journal.js
@@ -0,0 +1,17 @@
+const objectsJournalEntry = {
+    journalDate: "",
+    concepts: "",
+    journalEntry: "",
+    mood: ""
+}
+
+const allJournalEntries = [];
+
+allJournalEntries.push(objectsJournalEntry)
+
+/*function submit() {
+    let x = document.getElementById("myButton");
+    allJournalEntries.push(objectsJournalEntry)
+}
+
+console.table(allJournalEntries)*/
