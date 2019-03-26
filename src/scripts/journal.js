d09a3485368abaa9bcf2081711b6ec2206b25d41
Josh Hibray <jjhibray@gmail.com>
2019-03-25 23:25:21 -0500

    finished daily journal 3

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,13 +1,15 @@
-const objectsJournalEntry = {
-    journalDate: "",
-    concepts: "",
-    journalEntry: "",
-    mood: ""
-}
+//making these more dynamic in the DOM
 
-const allJournalEntries = [];
+// const objectsJournalEntry = {
+//     journalDate: "",
+//     concepts: "",
+//     journalEntry: "",
+//     mood: ""
+// }
 
-allJournalEntries.push(objectsJournalEntry)
+// const allJournalEntries = [];
+
+// allJournalEntries.push(objectsJournalEntry)
 
 /*function submit() {
     let x = document.getElementById("myButton");
@@ -15,3 +17,44 @@ allJournalEntries.push(objectsJournalEntry)
 }
 
 console.table(allJournalEntries)*/
+
+const journalEntries = [
+    {
+        concept: "Array methods",
+        date: "07/24/2018",
+        entry: "We learned about array methods, but only forEach made sense",
+        mood: "Ok"
+    }
+]
+
+const makeJournalEntryComponent = (journalEntries) => {
+    // Create your own HTML structure for a journal entry
+    return `
+    <p>${journalEntries.concept}<p>
+    <p>${journalEntries.date}<p>
+    <p>${journalEntries.entry}<p>
+    <p>${journalEntries.mood}<p>
+
+
+    `
+};
+
+
+    // Purpose: To render all journal entries to the DOM
+
+    // Arguments: entries (array of objects)
+
+    addArticleToDom = document.querySelector(".entryLog");
+
+const renderJournalEntries = (entries) => {
+    entries.forEach(entry => {
+        addArticleToDom.innerHTML += makeJournalEntryComponent(entry)
+
+    });
+
+};
+
+// Invoke the render function
+
+renderJournalEntries(journalEntries)
+
